2234b5c7b8de492d32be013e9d3f9edcaf478563
karthik449213 <karthikpiinasi@gmail.com>
2026-07-18 15:21:53 +0530

    Added events

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-    fflush(stdout);
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         printf("SDL_Init failed: %s\n", SDL_GetError());
@@ -11,24 +10,35 @@ int main(int argc, char *argv[])
     }
 
     printf("SDL INIT OK\n");
-    fflush(stdout);
 
     SDL_Window *window = SDL_CreateWindow(
-        "SDL3 Test",
+        "SDL3 audio player",
         800,
         600,
         0
     );
+
 
     if (!window) {
         printf("Window failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    printf("WINDOW CREATED\n");
-    fflush(stdout);
+    SDL_Event event;
+    bool running = true;
+    while(running)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+            
+        }
 
-    SDL_Delay(5000);
+    }
+
 
     SDL_DestroyWindow(window);
     SDL_Quit();
